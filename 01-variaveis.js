1bd232265d4e85217b47df0c4a53b0c759795b22
Emanuelly <manulimatotum@gmail.com>
2024-02-07 11:26:02 -0300

    javascript - basico

---
+++ 01-variaveis.js
@@ -0,0 +1,11 @@
+//Criando variáveis com "var" (var nome variavel = <valor>;)
+
+var nome = "Emanuelly";
+var idade = 16;
+
+console.log("Meu nome é", nome, "e tenho", idade, "anos!");
+console.log("Agora fiz aniversário! Tenho", idade+1, "anos!");
+
+//Aumentado automaticamente +1 (autoincremento)
+idade++ //idade = idade+1
+console.log("Agora fiz aniversário! Tenho", idade, "anos!")
---
+++ 02-aritmetico.js
@@ -0,0 +1,30 @@
+//Utilizando operadores aritméticos com JavaScript
+
+console.log("A soma entre 8 e 7 é:", 8 + 7);
+
+//Efetuar a soma de dois números informados por você (var nome variável = 12)
+var x = 10;
+var y = 5;
+console.log("A soma entre", x, "e", y, "é", x + y);
+console.log("A subtração entre", x, "e", y, "é", x - y);
+console.log("A multiplicação entre", x, "e", y, "é", x * y);
+console.log("A divisão entre", x, "e", y, "é:", x / y);
+
+var soma = x + y;
+var subtracao = x - y;
+var multiplicacao = x * y;
+var divisao = x / y;
+
+console.log("");
+console.log("soma = ", soma);
+console.log("subtracao = ", subtracao);
+console.log("multiplicacao = ", multiplicacao);
+console.log("divisao = ", divisao);
+
+//Regras para nomear identificadores (var, const, let)
+//Podem começar com A-Z, a-z, _ ou $
+
+var MinhaVariavel = 10
+var outraVariavel = 20
+const _minhaConstante = 3.1514
+var $minhaVariavel = 11
