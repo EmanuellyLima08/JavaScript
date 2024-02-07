34562207e775d4c59fbbbbf33a2ea6cd5b8dbd81
Emanuelly <manulimatotum@gmail.com>
2024-02-07 14:55:51 -0300

    conclusao da atividade 01

---
+++ emanuelly-atividade01.js
@@ -0,0 +1,27 @@
+var nome = "Emanuelly";
+var banco = "Itau";
+var agencia = 9901;
+var conta = 87023511;
+var saldo = 700;
+console.log("Olá,", nome, "!", "Seja bem-vinda ao", banco, ", cujo o número de agência é", agencia,". Informamos que na conta de número", conta, "há R$", saldo);
+console.log("")
+
+saldo = saldo + 200;
+console.log("Você recebeu um pix de R$200,00! Agora há R$", saldo, "em sua conta", banco, "!");
+
+saldo = saldo - 40;
+console.log("Você efetuou um pagamento de R$40,00! Agora há R$", saldo, "em sua conta", banco, "!");
+
+saldo = saldo + 3000;
+console.log("Seu pagamento caiu! Agora há R$", saldo, "em sua conta", banco, "!");
+
+saldo = saldo - 500;
+console.log("Você efetuou uma transferência no valor de R$500,00! Agora há", saldo, "em sua conta", banco, "!");
+
+saldo = saldo + 300
+console.log("Você recebeu um bônus de R$300,00! Agora há R$", saldo, "em sua conta", banco, "!");
+console.log("")
+
+console.log("Olá,", nome, "!", "Seja bem-vinda ao", banco, ", cujo o número de agência é", agencia, ". Informamos que na conta de número", conta, "há R$", saldo);
+
+
