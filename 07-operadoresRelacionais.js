140ec9a98042a4d84d8d8af477c799d1f7958ca0
Emanuelly <manulimatotum@gmail.com>
2024-02-20 13:52:14 -0300

    Entrega da atividade avaliativa 2

---
+++ 07-operadoresRelacionais.js
@@ -0,0 +1,24 @@
+//Operadores Relacionais
+var x = 22
+var y = 5
+console.log("----------------------------------------------");
+console.log("x =", x,"| y =", y);
+
+console.log("x é igual a y? |", x == y); // comparação
+console.log("x é diferente de y? |", x !== y); // != é diferente
+console.log("x é maior que y? |", x > y); //maior
+console.log("x é menor que y? |", x < y); //menor
+console.log("x é maior ou igual que y? |", x >= y); // >= maior ou igual que
+console.log("x é menor ou igual que y? |", x <= y); // <= menor ou igual que
+
+x=5
+console.log("----------------------------------------------");
+console.log("x =", x,"| y =", y);
+
+console.log("x é igual a y? |", x == y); // comparação
+console.log("x é diferente de y? |", x !== y); // != é diferente
+console.log("x é maior que y? |", x > y); //maior
+console.log("x é menor que y? |", x < y); //menor
+console.log("x é maior ou igual que y? |", x >= y); // >= maior ou igual que
+console.log("x é menor ou igual que y? |", x <= y); // <= menor ou igual que
+
---
+++ emanuelly-atividade02.js
@@ -0,0 +1,52 @@
+// Variáveis e Constantes
+// Atividade 1
+var nome = "Emanuelly";
+var anoNascimento = 2008;
+console.log("A", nome, "nasceu em", anoNascimento);
+
+var anoHoje = 2024;
+var idade = anoHoje - anoNascimento;
+console.log(nome, "tem", idade, "anos");
+
+// Atividade 2
+console.log("------------------------------------------------------------------");
+var largura = 6;
+var comprimento = 12;
+var area = largura * comprimento;
+console.log(
+  "A área de um retângulo de largura",
+  largura,
+  "cm e de comprimento",
+  comprimento,
+  "cm é igual a",
+  area,
+  "cm²"
+);
+
+// Atividade 3
+console.log("-----------------------------------------------------------------------");
+var altura = 5
+var volume = area * altura
+console.log("Se a altura deste retângulo fosse", altura, "cm, o seu volume seria", volume, "cm³")
+
+// Atividade 4 
+console.log("--------------------------------------------------------------------------");
+var a = 2
+var b = 3
+var c = -1
+var delta = b**2 -4 * a * c
+console.log("O valor de delta é", delta)
+
+// Atividade 5
+console.log("----------------------------------------------------------------------------");
+var compra = 450
+var desconto = compra / 100 * 3
+var valorFinal = compra - desconto
+console.log("O valor do desconto é R$", desconto, "| O valor final da compra é R$", valorFinal);
+
+// Atividade 6
+console.log("--------------------------------------------------------------------------------");
+var velMedia = 95
+var horas = 3.5
+var distancia = velMedia * horas
+console.log("A distância percorrida foi de", distancia, "km")
